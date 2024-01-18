3561d4250ce7a6e1e8fa83c5f6ba5be2f1a51ad5
Saood Imran <90656881+SaoodImran123@users.noreply.github.com>
2024-01-18 16:18:19 -0500

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -3,6 +3,7 @@ class Solution:
         target = 0
         targets = []
         solutions = set()
+        nums = sorted(nums)
         for index in range(0,len(nums)):
             dict = {}
             target =  0 - nums[index]
@@ -12,8 +13,11 @@ class Solution:
                 targets.append(target)
             for y in range(index+1,len(nums)):
                 if nums[y] in dict and y != dict[nums[y]]:
-                    tempSolution = tuple(sorted((nums[index],nums[dict[nums[y]]],nums[y])))
+                    tempSolution = (nums[index],nums[dict[nums[y]]],nums[y])
                     solutions.add(tempSolution)
                 requisite = target-nums[y]
                 dict[requisite] = y
+                if nums[index] + nums[y] > target:
+                    break
+
         return list(solutions)
